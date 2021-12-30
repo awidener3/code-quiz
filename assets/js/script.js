9285807da6209329531bf45f68ca7ea985eacd8f
Alex Widener <awidener3@gmail.com>
2021-12-29 21:20:20 -0500

    created a scoreboard that is able to take in the input and push on the end of the array

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,8 @@ var questions = [
     'question 4'
 ];
 
+var scoreboard = [['SJ', 40], ['DR', 3], ['YRM', 600]];
+
 var answers = [
     [['Q1 answer 1', true], ['Q1 answer 2', false], ['Q1 answer 3', false], ['Q1 answer 4', false]],
     [['Q2 answer 1', false], ['Q2 answer 2', true], ['Q2 answer 3', false], ['Q2 answer 4', false]],
@@ -24,6 +26,8 @@ startBtn.addEventListener("click", function () {
     initializeTimer();
     printQuestion();
 });
+
+// TIMER HANDLING
 
 function initializeTimer() {
     if (!timerInterval) {
@@ -41,6 +45,8 @@ function stopTime() {
     timerInterval = null;
 }
 
+// QUIZ HANDLING
+
 function printQuestion() {
     // check if there are any questions remaining in the questions array
     if (questions.length === 0) {
@@ -55,16 +61,16 @@ function printQuestion() {
 
     var answerList = document.createElement('ol');
 
-    answerList.appendChild(createAnswerLi(randomNum, 0));
-    answerList.appendChild(createAnswerLi(randomNum, 1));
-    answerList.appendChild(createAnswerLi(randomNum, 2));
-    answerList.appendChild(createAnswerLi(randomNum, 3));
+    answerList.appendChild(createAnswerChoice(randomNum, 0));
+    answerList.appendChild(createAnswerChoice(randomNum, 1));
+    answerList.appendChild(createAnswerChoice(randomNum, 2));
+    answerList.appendChild(createAnswerChoice(randomNum, 3));
 
     mainEl.appendChild(questionTitle);
     mainEl.appendChild(answerList);
 }
 
-function createAnswerLi(randomNum, index) {
+function createAnswerChoice(randomNum, index) {
     var answer = document.createElement('li');
     answer.classList.add('answer-choice');
     answer.addEventListener('click', checkAnswer);
@@ -82,6 +88,7 @@ function checkAnswer() {
         mainEl.textContent = '';
         printQuestion();
     } else {
+        this.textContent = this.textContent + ' ' + '❌';
         timer += 15;
     }
 }
@@ -89,11 +96,83 @@ function checkAnswer() {
 function endQuiz() {
     stopTime();
 
-    var title = document.createElement('h1')
-    title.textContent = 'quiz over!';
+    var affirmations = ['Keep it up, pal!', 'You\'re doing great!', 'I bet you could do this with your eyes closed!', 'I\'m sure everyone would be impressed if they saw you take this quiz!', 'Steve Jobs? Is that you?!', 'Excelsior!']
+
+    var title = document.createElement('h1');
+    title.textContent = 'Quiz Over!';
+
+    var results = document.createElement('p');
+    results.textContent = 'You scored ' + timer + ' points. ' + affirmations[randomNumber(affirmations.length)];
+
+    var enterInitials = document.createElement('p');
+    enterInitials.textContent = 'Please enter your initials.'
+
+    var initialsInput = document.createElement('input');
+    initialsInput.maxLength = 2;
+    initialsInput.maxLength = 3;
+    initialsInput.size = 4;
+    initialsInput.required = true;
+
+    var button = document.createElement('button');
+    button.textContent = 'Go to Highscores';
+
+    button.addEventListener('click', function() {
+        console.log(initialsInput.value);
+
+        if (!initialsInput.value) {
+            console.log('nothing to see here...')
+        } else {
+            var playerInitials = initialsInput.value;
+            var playerScore = timer;
+            var playerInfo = [playerInitials, playerScore]
+            printHighscores(playerInfo);
+        }
+    })
 
     mainEl.appendChild(title);
+    mainEl.appendChild(results);
+    mainEl.appendChild(enterInitials);
+    mainEl.appendChild(initialsInput);
+    mainEl.appendChild(button);
 };
+
+function printHighscores(playerInfo) {
+    scoreboard.push(playerInfo);
+
+    sortScoreboard();
+    
+    mainEl.textContent = '';
+
+    var title = document.createElement('h1');
+    title.textContent = 'Leaderboard';
+
+    mainEl.appendChild(title);
+
+    var scores = document.createElement('div');
+    scores.setAttribute('id', 'scoreboard');
+
+    for (var i = 0; i < scoreboard.length; i++) {
+        printPlayer(i, scores);
+    }
+
+    mainEl.appendChild(scores);
+}
+
+function printPlayer(index, scores) {
+    var row = document.createElement('div');
+    row.classList.add('score-row');
+
+    var playerInitials = document.createElement('p');
+    playerInitials.textContent = scoreboard[index][0];
+
+    var playerScore = document.createElement('p');
+    playerScore.textContent = scoreboard[index][1];
+
+    row.appendChild(playerInitials);
+    row.appendChild(playerScore);
+
+    scores.appendChild(row);
+}
 
 function randomNumber(max) {
     return Math.floor(Math.random() * max);
